3bc0a02028577475246bd17d52711a0bdc6e2fa1
Josh Cook <joshuacook0023@gmail.com>
2019-09-20 20:39:43 -0400

    start Xcode project

---
+++ Germination Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Germination Tracker
+//
+//  Created by Joshua on 9/20/19.
+//  Copyright © 2019 Joshua Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
