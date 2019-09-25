207fe533ab97b72379a8b1f914256b5103176a97
Josh Cook <joshuacook0023@gmail.com>
2019-09-25 07:39:12 -0400

    feat: germination plot is up and running

--- Germination Tracker/View Controllers/ChartViewController.swift
+++ Germination Tracker/View Controllers/ChartViewController.swift
@@ -36,48 +36,122 @@ class ChartViewController: UIViewController {
     
     func setGerminationLineChart() {
         
-        // let xVals = plant.seedGerminationDates
-        /// TODO: count germinations cumulatively
+        let secondsInADay = 60 * 60 * 24
         
-        // Test data
-        let count = 100
-        let range: UInt32 = 30
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
         
+        var startDate = plant.dateOfSeedSowing
+        let nowDate = Date()
+        
+        // break early if today is the same day at start of germination
+        if Calendar.current.isDate(startDate, inSameDayAs: nowDate) {
+            germinationLineChartView.noDataText = "Good luck!"
+            return
+        }
+        
+        // x-values for plot
+        var allDatesSinceBeginning  = [Date]()
+        while startDate <= nowDate {
+            allDatesSinceBeginning.append(startDate)
+            startDate = Calendar.current.date(byAdding: .day, value: 1, to: startDate)!
+        }
+        
+        
+        // y-values for plot
+        var cumulativeGerminationCount = [Double]()
+        var totalGermCount: Double = 0
+        for day in allDatesSinceBeginning {
+            var dayGermCount: Double = 0
+             for germDate in plant.seedGerminationDates {
+                if Calendar.current.isDate(day, inSameDayAs: germDate) { dayGermCount += 1 }
+            }
+            totalGermCount += dayGermCount
+            cumulativeGerminationCount.append(totalGermCount)
+        }
+        
+        
+        if allDatesSinceBeginning.count != cumulativeGerminationCount.count {
+            fatalError("The number of dates (x) does not equal the number of cumulative germ. counts (y).")
+        }
+        
+        // turn into chart data entry
         let now = Date().timeIntervalSince1970
-        let hourSeconds: TimeInterval = 3600
-        let from = now - (Double(count) / 2) * hourSeconds
-        let to = now + (Double(count) / 2) * hourSeconds
-        let values = stride(from: from, to: to, by: hourSeconds).map { (x) -> ChartDataEntry in
-        let y = arc4random_uniform(range) + 50
-        return ChartDataEntry(x: x, y: Double(y))
-                }
-        let set1 = LineChartDataSet(entries: values, label: "DataSet 1")
-        set1.axisDependency = .left
-        set1.setColor(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
-        set1.lineWidth = 1.5
-        set1.drawCirclesEnabled = false
-        set1.drawValuesEnabled = false
-        set1.fillAlpha = 0.26
-        set1.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
-        set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        var values = [ChartDataEntry]()
+        for i in 1..<cumulativeGerminationCount.count {
+            let x = (now - allDatesSinceBeginning[i].timeIntervalSince1970) / Double(secondsInADay)
+            values.append(ChartDataEntry(x: x.rounded(.towardZero), y: cumulativeGerminationCount[i]))
+        }
+        
+        values = values.reversed()
+        
+        let set1 = LineChartDataSet(entries: values, label: "Germinations")
+        
+        // customize set1
+        set1.drawCirclesEnabled = true
         set1.drawCircleHoleEnabled = false
+        set1.drawValuesEnabled = true
+        set1.lineWidth = 2
+        set1.axisDependency = .left
+        set1.fillColor = FlatMintDark()
         
         let data = LineChartData(dataSet: set1)
-        data.setValueTextColor(.white)
-        data.setValueFont(.systemFont(ofSize: 9, weight: .light))
-        
         germinationLineChartView.data = data
+        
+        germinationLineChartView.animate(xAxisDuration: 1.0, yAxisDuration: 1.0, easingOption: .easeInCubic)
+        
+        
+//        // Test data
+//        let count = 100
+//        let range: UInt32 = 30
+//
+//        let now = Date().timeIntervalSince1970
+//        let hourSeconds: TimeInterval = 3600
+//        let from = now - (Double(count) / 2) * hourSeconds
+//        let to = now + (Double(count) / 2) * hourSeconds
+//        let values = stride(from: from, to: to, by: hourSeconds).map { (x) -> ChartDataEntry in
+//            let y = arc4random_uniform(range) + 50
+//            return ChartDataEntry(x: x, y: Double(y))
+//        }
+//        let set1 = LineChartDataSet(entries: values, label: "DataSet 1")
+//        set1.axisDependency = .left
+//        set1.setColor(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
+//        set1.lineWidth = 1.5
+//        set1.drawCirclesEnabled = false
+//        set1.drawValuesEnabled = false
+//        set1.fillAlpha = 0.26
+//        set1.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
+//        set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+//        set1.drawCircleHoleEnabled = false
+//
+//        let data = LineChartData(dataSet: set1)
+//        data.setValueTextColor(.white)
+//        data.setValueFont(.systemFont(ofSize: 9, weight: .light))
+//
+//        germinationLineChartView.data = data
     }
+
+}
+
+
+
+extension ChartViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getTestDates() -> [Date] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let testGerminationDates: [Date] = [
+            dateFormatter.date(from: "09/20/2019")!,
+            dateFormatter.date(from: "09/20/2019")!,
+            dateFormatter.date(from: "09/20/2019")!,
+            dateFormatter.date(from: "09/21/2019")!,
+            dateFormatter.date(from: "09/22/2019")!,
+            dateFormatter.date(from: "09/22/2019")!,
+            dateFormatter.date(from: "09/22/2019")!,
+            dateFormatter.date(from: "09/23/2019")!,
+            dateFormatter.date(from: "09/23/2019")!,
+            dateFormatter.date(from: "09/23/2019")!
+        ]
+        return testGerminationDates
     }
-    */
-
 }
