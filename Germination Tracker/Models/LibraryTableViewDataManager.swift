4546a4fad980b5012267a87c1049cedad41cf171
Josh Cook <joshuacook0023@gmail.com>
2019-11-05 17:43:08 -0500

    documentation for LibraryTableViewDataManager

--- Germination Tracker/Models/LibraryTableViewDataManager.swift
+++ Germination Tracker/Models/LibraryTableViewDataManager.swift
@@ -33,17 +33,26 @@ enum OrderDirection {
     case ascending, descending
 }
 
-
+/**
+ A single group for the library table view. Each has a title and an array of the plants that make up the
+ rows for the section. Everything is passed by reference. Therefore, the changes are propogated to the
+ other locations of the plant.
+ */
 struct GroupedSection {
 
+    /// Title of the section.
     var sectionName : String
+    /// The plants that make up the rows of the section.
     var rows : [Plant]
 
+    /// Sort the plants by name.
     mutating func sortByName() {
         rows.sort { $0.name < $1.name }
     }
     
     
+    /// Sort the plants by date.
+    /// - Parameter direction: Should the plants be sorted in ascending or descending order?
     mutating func sortPlantsByDate(_ direction: OrderDirection) {
         switch direction {
         case .ascending:
@@ -56,18 +65,35 @@ struct GroupedSection {
 
 
 
+/**
+ A special data manager for the library table view, controlled by `LibraryViewController`.
+ It adjusts the sections for the different sorting options (enumerated in `SortOption`). The options to sort
+ by date are all in one section, but the options for  sorting by name or active and inactive are in different sections.
+ There are a bunch of helper functions for queiring the sections. For instance, the index for a plant or the plant
+ for an index can be requested.
+ */
 class LibraryTableViewDataManager {
     
+    /// The plants manager handling the core array of plants for the app.
     var plantsManager: PlantsArrayManager
     
+    /// The sorted array of sections. Each section has a title and rows. The rows can be sorted by
+    /// date, too.
     var sections = [GroupedSection]()
     
+    /// The method to use for sorting. Setting this variable automatically triggers a reorganization. Therefore,
+    /// it is recommended to make sure the user has actually requested a different sorting option before
+    /// setting this value.
     var sortOption: SortOption {
         didSet {
             organizeSections()
         }
     }
     
+    
+    /// Initialize the data manager.
+    /// - Parameter plantsManager: The central plants manager for the app.
+    /// - Parameter sortOption: The method for sorting.
     init(plantsManager: PlantsArrayManager, sortOption: SortOption) {
         os_log("Initializing a library data manager", log: Log.libraryDM, type: .info)
         self.plantsManager = plantsManager
@@ -76,6 +102,7 @@ class LibraryTableViewDataManager {
     }
     
     
+    /// 
     func organizeSections() {
         os_log("Organizing sections.", log: Log.libraryDM, type: .info)
         switch sortOption {
