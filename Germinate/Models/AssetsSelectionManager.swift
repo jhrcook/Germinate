c77367e9b176d4ce05c231f8ed3d4908ae700914
Josh Cook <joshuacook0023@gmail.com>
2019-11-24 12:00:15 -0800

    New model class to coordinate the selection of assets and their downloading

---
+++ Germinate/Models/AssetsSelectionManager.swift
@@ -0,0 +1,13 @@
+//
+//  AssetsSelectionManager.swift
+//  Germinate
+//
+//  Created by Joshua on 11/24/19.
+//  Copyright © 2019 Joshua Cook. All rights reserved.
+//
+
+import UIKit
+
+class AssetsSelectionManager: NSObject {
+
+}
